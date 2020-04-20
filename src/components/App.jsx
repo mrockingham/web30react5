1a80c84cb7339e99de23cd31ef2151e184f35ced
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-20 19:02:35 +0200

    wip

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from 'react'
+// STEP 2 - import Route, Switch and Link from react router
+import { Route, Switch, Link } from 'react-router-dom'
+
 import axios from 'axios'
 
 // these will render conditionally depending on URL
@@ -24,10 +27,15 @@ export default function App() {
   return (
     <div className='container'>
       <header><h1>Friends App</h1><a href='/'>Home</a></header>
+      {/* STEP 3 WRAP COMPONENTS IN ROUTES */}
+      {/* the path props is what needs to 'match' in the url in order to render */}
+      <Route path='/'>
+        <FriendsList friends={friends} />
+      </Route>
 
-      <FriendsList friends={friends} />
-
-      <Friend />
+      <Route path='/friends/:id'>
+        <Friend />
+      </Route>
     </div>
   )
 }
